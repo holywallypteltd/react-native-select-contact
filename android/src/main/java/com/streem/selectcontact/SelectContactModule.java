9219401922ead3e0cd9c122b35e88860fbd586ad
danilofatcat <danilo.gacevic@fatcatcoders.com>
2021-12-22 14:41:14 +0100

    fix contact select for android

--- android/src/main/java/com/streem/selectcontact/SelectContactModule.java
+++ android/src/main/java/com/streem/selectcontact/SelectContactModule.java
@@ -5,6 +5,7 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.provider.ContactsContract.CommonDataKinds.Email;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.provider.ContactsContract.CommonDataKinds.StructuredName;
@@ -12,6 +13,7 @@ import android.provider.ContactsContract.CommonDataKinds.StructuredPostal;
 import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.Contacts.Entity;
 import android.util.Log;
+import android.content.Context;
 
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
@@ -22,10 +24,13 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 
+import java.util.ArrayList;
+
 public class SelectContactModule extends ReactContextBaseJavaModule implements ActivityEventListener {
 
     private static final String TAG = "SelectContactModule";
     private static final int CONTACT_REQUEST = 11112;
+    private static final int REQUEST_CODE = 1;
     public static final String E_CONTACT_CANCELLED = "E_CONTACT_CANCELLED";
     public static final String E_CONTACT_NO_DATA = "E_CONTACT_NO_DATA";
     public static final String E_CONTACT_EXCEPTION = "E_CONTACT_EXCEPTION";
@@ -47,7 +52,17 @@ public class SelectContactModule extends ReactContextBaseJavaModule implements A
 
     @ReactMethod
     public void openContactSelection(Promise contactsPromise) {
-        launchPicker(contactsPromise, CONTACT_REQUEST);
+        mContactsPromise = contactsPromise;
+
+
+        Uri uri = Uri.parse("content://contacts");
+        Intent intent = new Intent(Intent.ACTION_PICK, uri);
+        intent.setType(Phone.CONTENT_TYPE);
+        Activity activity = getCurrentActivity();
+
+        activity.startActivityForResult(intent, REQUEST_CODE);
+
+
     }
 
     /**
@@ -59,7 +74,7 @@ public class SelectContactModule extends ReactContextBaseJavaModule implements A
     private void launchPicker(Promise contactsPromise, int requestCode) {
         mContactsPromise = contactsPromise;
         Cursor cursor = this.contentResolver.query(Contacts.CONTENT_URI, null, null, null, null);
-        
+
         if (cursor != null) {
             Intent intent = new Intent(Intent.ACTION_PICK);
             intent.setType(Contacts.CONTENT_TYPE);
@@ -73,76 +88,29 @@ public class SelectContactModule extends ReactContextBaseJavaModule implements A
         }
     }
 
-    @Override
-    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent intent) {
-        if (mContactsPromise == null || requestCode != CONTACT_REQUEST) {
-            return;
-        }
-
-        //Request was cancelled
-        if (resultCode != Activity.RESULT_OK) {
-            mContactsPromise.reject(E_CONTACT_CANCELLED, "Cancelled");
-            return;
-        }
-
-        // Retrieve all possible data about contact and return as a JS object
-        WritableMap contactData = Arguments.createMap();
-
-        try {
-            String id = getContactId(intent.getData());
-            contactData.putString("recordId", id);
-            Uri contactUri = buildContactUri(id);
-            boolean foundData = false;
-
-            WritableArray phones = Arguments.createArray();
-            WritableArray emails = Arguments.createArray();
-            WritableArray postalAddresses = Arguments.createArray();
-
-            Cursor cursor = openContactQuery(contactUri);
-            if (cursor.moveToFirst()) {
-                do {
-                    String mime = cursor.getString(cursor.getColumnIndex(Entity.MIMETYPE));
-                    switch (mime) {
-                        case StructuredName.CONTENT_ITEM_TYPE:
-                            addNameData(contactData, cursor);
-                            foundData = true;
-                            break;
-
-                        case StructuredPostal.CONTENT_ITEM_TYPE:
-                            addPostalData(postalAddresses, cursor, activity);
-                            foundData = true;
-                            break;
-
-                        case Phone.CONTENT_ITEM_TYPE:
-                            addPhoneEntry(phones, cursor, activity);
-                            foundData = true;
-                            break;
-
-                        case Email.CONTENT_ITEM_TYPE:
-                            addEmailEntry(emails, cursor, activity);
-                            foundData = true;
-                            break;
-                    }
-                } while (cursor.moveToNext());
-            }
-            cursor.close();
-
-            contactData.putArray("phones", phones);
-            contactData.putArray("emails", emails);
-            contactData.putArray("postalAddresses", postalAddresses);
-
-            if (foundData) {
-                mContactsPromise.resolve(contactData);
-            } else {
-                mContactsPromise.reject(E_CONTACT_NO_DATA, "No data found for contact");
-            }
-        } catch (SelectContactException e) {
-            mContactsPromise.reject(E_CONTACT_EXCEPTION, e.getMessage());
-        } catch (Exception e) {
-            Log.e(TAG, "Unexpected exception reading from contacts", e);
-            mContactsPromise.reject(E_CONTACT_EXCEPTION, e.getMessage());
-        }
-    }
+//     @Override
+//     protected void onActivityResult(int requestCode, int resultCode,
+//             Intent intent) {
+//         if (requestCode == REQUEST_CODE) {
+//             if (resultCode == Activity.RESULT_OK) {
+//                 Uri uri = intent.getData();
+//                 String[] projection = { Phone.NUMBER, Phone.DISPLAY_NAME };
+//
+//                 Cursor cursor = getReactApplicationContext().getContentResolver().query(uri, projection,
+//                         null, null, null);
+//                 cursor.moveToFirst();
+//
+//                 int numberColumnIndex = cursor.getColumnIndex(Phone.NUMBER);
+//                 String number = cursor.getString(numberColumnIndex);
+//
+//                 int nameColumnIndex = cursor.getColumnIndex(Phone.DISPLAY_NAME);
+//                 String name = cursor.getString(nameColumnIndex);
+//
+//                 Log.d(TAG, "ZZZ number : " + number +" , name : "+name);
+//
+//             }
+//         }
+//     };
 
     private String getContactId(Uri contactUri) throws SelectContactException {
         Cursor cursor = this.contentResolver.query(contactUri, null, null, null, null);
@@ -269,6 +237,148 @@ public class SelectContactModule extends ReactContextBaseJavaModule implements A
     }
 
     @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent intent) {
+        if (mContactsPromise == null || requestCode != 1) {
+            return;
+        }
+        if (resultCode != Activity.RESULT_OK) {
+            mContactsPromise.reject(E_CONTACT_CANCELLED, "Cancelled");
+            return;
+        }
+        // Retrieve all possible data about contact and return as a JS object
+        WritableMap contactData = Arguments.createMap();
+
+        try {
+            Uri uri = intent.getData();
+            boolean foundData = false;
+
+
+            Cursor cursor = getCurrentActivity().getContentResolver().query(uri, null, null, null, null);
+            if (cursor == null) {
+                mContactsPromise.reject(E_CONTACT_PERMISSION, "no permission");
+                return;
+            }
+            ContentResolver cr = getCurrentActivity().getContentResolver();
+
+            if (null != cursor && cursor.getCount() > 0) {
+                cursor.moveToFirst();
+                for (String column : cursor.getColumnNames()) {
+                    Log.i(TAG, "contactPicked() uri column " + column + " : " + cursor.getString(cursor.getColumnIndex(column)));
+                }
+            }
+
+            cursor.moveToFirst();
+            String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+
+            Log.i(TAG, "contactPicked() uri id " + id);
+            String contact_id = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID));
+            Log.i(TAG, "contactPicked() uri contact id " + contact_id);
+            // column index of the contact name
+            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            // column index of the phone number
+            String phoneNo = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            //get Email id of selected contact....
+            Log.e("ContactsFragment", "::>> " + id + name + phoneNo);
+
+            Cursor cur1 = cr.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+                    ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?", new String[]{contact_id}, null);
+            String email = null;
+            if (null != cur1 && cur1.getCount() > 0) {
+                cur1.moveToFirst();
+                for (String column : cur1.getColumnNames()) {
+                    Log.i(TAG, "contactPicked() Email column " + column + " : " + cur1.getString(cur1.getColumnIndex(column)));
+                    email = cur1.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                }
+
+                //HERE YOU GET name, phoneno & email of selected contact from contactlist.....
+                Log.e("setcontactDetails", "::>>" + name + "\nPhoneno:" + phoneNo + "\nEmail: " + email);
+            } else {
+                Log.e("setcontactDetails", "::>>" + name + "\nPhoneno:" + phoneNo + "\nEmail: " + email);
+            }
+            WritableArray phones = Arguments.createArray();
+            WritableArray emails = Arguments.createArray();
+            WritableMap phoneEntry = Arguments.createMap();
+            phoneEntry.putString("number", phoneNo);
+            phones.pushMap(phoneEntry);
+
+            WritableMap emailEntry = Arguments.createMap();
+            emailEntry.putString("address", email);
+            contactData.putString("recordId", id);
+
+            contactData.putArray("phones", phones);
+            mContactsPromise.resolve(contactData);
+
+
+//
+//            Cursor cursor = contentResolver.query(
+//                    Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI, ContactsContract.Contacts.Data.CONTENT_DIRECTORY),
+//                    null,
+//                    null,
+//                    null,
+//                    null
+//            );
+//
+//            if (true) {
+//                do {
+//                    String mime = cursor.getString(cursor.getColumnIndex(ContactsContract.Data.CONTACT_ID));
+//                    switch (mime) {
+//                        case StructuredName.CONTENT_ITEM_TYPE:
+//                            addNameData(contactData, cursor);
+//                            foundData = true;
+//                            break;
+//
+//                        case StructuredPostal.CONTENT_ITEM_TYPE:
+//                            addPostalData(postalAddresses, cursor, activity);
+//                            foundData = true;
+//                            break;
+//
+//                        case Phone.CONTENT_ITEM_TYPE:
+//                            addPhoneEntry(phones, cursor, activity);
+//                            foundData = true;
+//                            break;
+//
+//                        case Email.CONTENT_ITEM_TYPE:
+//                            addEmailEntry(emails, cursor, activity);
+//                            foundData = true;
+//                            break;
+//                    }
+//                } while (cursor.moveToNext());
+//            }
+//            cursor.close();
+//
+//            contactData.putArray("phones", phones);
+//            contactData.putArray("emails", emails);
+//            contactData.putArray("postalAddresses", postalAddresses);
+//
+//            if (foundData) {
+//                mContactsPromise.resolve(contactData);
+//            } else {
+//                mContactsPromise.reject(E_CONTACT_NO_DATA, "No data found for contact");
+//            }
+        } catch (Exception e) {
+            Log.e(TAG, "Unexpected exception reading from contacts", e);
+            mContactsPromise.reject(E_CONTACT_EXCEPTION, e.getMessage());
+        }
+    }
+
+    public ArrayList<String> getNameUsingContactId(String id) {
+
+        ArrayList<String> phones = new ArrayList<String>();
+
+        Cursor cursor = getReactApplicationContext().getContentResolver().query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                new String[]{id}, null);
+
+        while (cursor.moveToNext()) {
+            phones.add(cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+        }
+
+        cursor.close();
+        return (phones);
+    }
+
     public void onNewIntent(Intent intent) {
 
     }
